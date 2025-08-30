1cfb64d2154148805613b905597473c06462a4c7
jardeloli <jardelsantosa380@gmail.com>
2025-08-30 18:15:38 -0300

    funcionalidades do componente taskFilter

--- src/app/task-filter/task-filter.ts
+++ src/app/task-filter/task-filter.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-filter.html',
   styleUrl: './task-filter.css'
 })
 export class TaskFilter {
 
+  filter: string = '';
+
+  @Output() taskFilter = new EventEmitter<string>();
+
+  filterTask(){
+    this.taskFilter.emit(this.filter);
+  }
 }
